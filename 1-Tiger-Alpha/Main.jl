830f0e25bdb3c506a23a4b1c2b6efc2258b1ec46
Erfan Mirhaji <erfan.mirhaji@colostate.edu>
2024-07-26 13:05:45 -0600

    Now solver is QMDP

--- 1-Tiger-Alpha/Main.jl
+++
@@ -1,4 +0,0 @@
-using POMDPs
-using sockets
-
-
--- 1-Tiger-Alpha/TigerServer2.jl
+++ 1-Tiger-Alpha/TigerServer2.jl
@@ -1,10 +1,13 @@
 using Pkg
 Pkg.activate("/Users/erfan/OneDrive - Colostate/Julia/Envs/env1")
 
+using Sockets
 using POMDPs
 using POMDPModels
 using QMDP
 using POMDPTools
+
+
 
 pomdp = TigerPOMDP()
 solver = QMDPSolver()
@@ -26,10 +29,31 @@ function update_belief!(belief, observation)
     belief /= sum(belief)
 end
 
+
+
+HOST = "localhost"
+PORT = 2042
+server_socket = listen(PORT)
+println("Server listening on $HOST:$PORT")
+
+
+
 while true
-    println("Please enter observation: tiger_left or tiger_right")
-    obs = readline()
-    update_belief!(belief, obs)
-    policy_action = action(policy, belief)
-    println("Action for observation $obs: $policy_action")
+    client_socket = accept(server_socket)
+    println("Connected to client")
+    
+    while true
+        # This line receives:
+        obs = readline(client_socket)
+        println("Observation from client is: $obs")
+        
+        update_belief!(belief, obs)
+        policy_action = action(policy, belief)
+        
+        # This line transmits:
+        write(client_socket, "Take action: $policy_action\n")
+    end
+    close(client_socket)
 end
+
+
