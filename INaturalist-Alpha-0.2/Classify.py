0a7222b1a4ebc112b2468ef1f8a6c776bbd4dee8
Erfan Mirhaji <erfan.mirhaji@colostate.edu>
2024-08-13 16:54:00 -0600

    hi

--- INaturalist-Alpha-0.2/Classify.py
+++ INaturalist-Alpha-0.2/Classify.py
@@ -18,13 +18,13 @@ model_ft.eval()
 with open('./pollinators.txt') as f:
     class_names = f.read().splitlines()
 
-
 data_transforms = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
+
 
 def classify_image(image_path):
     image = Image.open(image_path).convert('RGB')
@@ -40,6 +40,12 @@ def classify_image(image_path):
     return predicted_class
 
 
-image_path = './test.jpg'  # Replace with your image path
+image_path = './test.jpg'
+predicted_class = classify_image(image_path)
+print(f'The predicted class is: {predicted_class}')
+image_path = './test.jpg'
+predicted_class = classify_image(image_path)
+print(f'The predicted class is: {predicted_class}')
+image_path = './test.jpg'
 predicted_class = classify_image(image_path)
 print(f'The predicted class is: {predicted_class}')
